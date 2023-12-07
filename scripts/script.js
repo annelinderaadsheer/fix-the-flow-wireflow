4e1628a3056148f0cc91222856c09cb08c6ed846
annelinderaadsheer <144004885+annelinderaadsheer@users.noreply.github.com>
2023-12-07 10:26:01 +0100

    JS toegepast
    Knop 'Verlengen' toegevoegd en JS toegepast met wat CSS styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
     toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box");
+    searchBtn = body.querySelector(".search-box")
+    btnExtends = document.querySelectorAll('.btn-extend');
+
+    console.log('btnExtend', btnExtends)
 
 
 toggle.addEventListener("click", () => {
@@ -12,3 +15,23 @@ searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 })
 
+// 1. Get the element
+// 2. Add event listener
+// 3. execute function if event is fired
+
+btnExtends.forEach((element) => {
+    element.addEventListener('click', () => {
+    // console.log('clicked element', element)
+    element.innerHTML = "U heeft verlengd";
+    element.classList.add('extended')
+  });
+})
+
+// tekstElement. veranderTekst() {
+//     var tekstElement = document.getElementById("tekstElement");
+//     tekstElement.innerHTML = "Verloopt over: 31 dagen";
+// }
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "U heeft verlengd";
+//       }
